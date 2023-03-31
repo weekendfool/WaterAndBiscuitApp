81c10edd10000a4eb205d327da3761ea10eb3e06
尾原徳泰 <nori.tukareta@gmail.com>
2023-04-01 03:08:25 +0900

    テスト

--- WaterAndBiscuit/WaterAndBiscuit/SkillView.swift
+++ WaterAndBiscuit/WaterAndBiscuit/SkillView.swift
@@ -8,15 +8,61 @@
 import SwiftUI
 
 struct SkillView: View {
-    var body: some View {
-        ZStack {
-            Color.black.ignoresSafeArea()
-            
-            Text("スキル")
-                .foregroundColor(.white)
-            
-        }
+//    var body: some View {
         
+        
+        @State var ripple = false
+        @State var bigDrop = false
+        @State var littleDrop = false
+        var dropSpeed = 0.45
+        var dropInterval = 2.0
+        
+        var body: some View {
+            ZStack{
+                
+                Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1))
+                
+                // Big Water Drop
+                Circle()
+                    .fill(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                    .frame(width:10,height:10)
+                    .offset(x:0, y:bigDrop ? 0 : -420)
+                    .animation(.linear(duration: dropSpeed).delay(dropInterval).repeatForever(autoreverses: false))
+                    .onAppear(){
+                        bigDrop.toggle()
+                    }
+                
+                // Ripple
+                ZStack(alignment: .center) {
+                    Circle()
+                        .stroke(lineWidth: ripple ? 0 : 30)
+                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                        .frame(width:300, height:300)
+                        .scaleEffect(ripple ? 1 : 0)
+                        .animation(Animation.easeOut(duration:(dropSpeed + dropInterval)).repeatForever(autoreverses: false).delay(dropSpeed + dropInterval))
+                }
+                .rotation3DEffect(
+                    .degrees(65),
+                    axis: (x: 1.0, y: 0.0, z: 0.0),
+                    anchor: .center,
+                    anchorZ: 0.0,
+                    perspective: 1.0
+                )
+                .onAppear(){
+                    ripple.toggle()
+                }
+            }.ignoresSafeArea(.all)
+//        }
+        //    }
+        //        ZStack {
+        //            Color.black.ignoresSafeArea()
+        //
+        //            Text("スキル")
+        //                .foregroundColor(.white)
+        //
+        //        }
+        //
+        //    }
     }
 }
 
